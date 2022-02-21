2f49feb52188c2996da5a74f8e3d8ee446e3ae81
Bishoy Osama <beshoyprototype@hotmail.com>
2022-02-21 19:41:56 +0200

    feat: add API key

--- website/app.js
+++ website/app.js
@@ -1,5 +1,8 @@
 /* Global Variables */
 
+// Personal API Key for OpenWeatherMap API
+const apiKey = '<your_api_key>&units=a86ddcd72bd031597483ce10ddb70d77';
+
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
