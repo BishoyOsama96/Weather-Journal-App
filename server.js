72c4fd18b97a1f2fad32972723924b93deca3074
Bishoy Osama <beshoyprototype@hotmail.com>
2022-02-22 00:39:43 +0200

    fix: bug fix

--- server.js
+++ server.js
@@ -7,16 +7,13 @@ const express = require('express');
 const app = express();
 
 /* Middleware*/
-const bodyParser = require('body-parser');
-//Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+//Here we are configuring express as middle-ware.
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // Cors for cross origin allowance
-const Cors = require('cors');
-const {json} = require('body-parser');
-const { request } = require('http');
-const { response } = require('express');
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
--- website/app.js
+++ website/app.js
@@ -5,12 +5,12 @@ const apiKey = '<your_api_key>&units=a86ddcd72bd031597483ce10ddb70d77';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth()+1+'.'+ d.getDate()+'.'+ d.getFullYear();
 
 // GET Route II: Client Side
 const getData = async (baseURL,cityZipCode,apiKey)=>{
     // generate url from the api
-    const response = await fetch(`${baseURL+cityZipCode+apiKey}`);
+    const response = await fetch(baseURL+cityZipCode+apiKey);
 
     try {
         // get data from the server and convert it to json
